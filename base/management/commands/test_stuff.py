49f5f541621f78968cf17d3b4dac4f5d7fb1a9c4
Amir <miramir1382@gmail.com>
2026-08-12 23:05:23 +0330

    orm training

--- base/management/commands/test_stuff.py
+++ base/management/commands/test_stuff.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.db.models import F, Q, Case, ExpressionWrapper, FloatField, Value, When
+from django.db.models import F, Q, Case, ExpressionWrapper, FloatField, OuterRef, Subquery, Value, When
 from django.db.models.aggregates import Count
 from base.models import Task
 from django.contrib.auth.models import User
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # Day1
+        # Day1 - annotate/Count/Case-When/ExpressionWrapper/F/
         # queryset = User.objects.annotate(total_tasks=Count('task', distinct=True),completed_tasks=Count('task', distinct=True, filter=Q(task__complete=True)),
         #                                  incomplete_tasks=Count('task', distinct=True, filter=Q(task__complete=False)),
         #                                  completion_percentage=Case(
@@ -19,12 +19,13 @@ class Command(BaseCommand):
         #                                 ).order_by('-total_tasks')
         
         # for user in queryset:
-        #     completion_percentage=(float(user.completed_tasks)/float(user.total_tasks))*100
         #     print(f'{user.username} \n tasks:\ntotal: {user.total_tasks}\ncompleted: {user.completed_tasks}\nincomplete: {user.incomplete_tasks}\n{completion_percentage}\n')
             
         # print(queryset.query)
         
-        #Day2
+        # ------------------------------------------------------------------------------------------
+        
+        #Day2 - Q
         # queryset = Task.objects.filter(Q(title__icontains='test') | Q(description__icontains='test') & Q(complete=False))
         # queryset = User.objects.annotate(total_tasks_count=Count('task'), 
         #                                  test_tasks_count=Count(
@@ -51,4 +52,31 @@ class Command(BaseCommand):
         #     ).filter(Q(active_message_count__gt=0) | Q(reaction_count__gt=5)
         #              ).order_by('-message_count', '-reaction_count')
         
-        #Day3
+        # ------------------------------------------------------------------------------------------
+        
+        #Day3 - Subquery/OuterRef
+        # latest_message_time = (
+        #     Message.objects
+        #     .filter(sender=OuterRef('pk'))
+        #     .order_by('-created_at')
+        #     .values('created_time')[:1]
+        # )
+
+        # users = User.objects.annotate(
+        #     last_message_time=Subquery(latest_message_time)
+        # )
+        
+        # last_message = Message.objects.filter(sender=OuterRef('pk')
+        #                                       ).order_by('-created_at')[:1]        
+        
+        # users = User.objects.annotate(
+        #     message_count = Count('messages'),
+        #     last_message_time = Subquery(last_message.values('created_at')),
+        #     last_message_content = Subquery(last_message.values('content')),
+        #     last_message_room_name = Subquery(last_message.values('room__name')),
+        # ).filter(message_count__gte=3
+        #     ).order_by('-last_message_time')
+        
+        # ------------------------------------------------------------------------------------------
+        
+        #Day4
